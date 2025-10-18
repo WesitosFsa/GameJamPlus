1f2b8bfd6454c92e6e1786cc33284234fbda6ccf
Mateo Garzon <mateogarzonfsa@gmail.com>
2025-10-18 12:08:27 -0500

    eventos

--- scripts/box.gd
+++ scripts/box.gd
@@ -1,4 +1,5 @@
 extends StaticBody3D
+@onready var audio_stream_player_3d: AudioStreamPlayer3D = $AudioStreamPlayer3D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,4 +12,10 @@ func _physics_process(delta: float) -> void:
 
 func mouse_interaction():
 	if Input.is_action_just_pressed("E"):
-		queue_free()
+		$AudioStreamPlayer3D.play()
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		audio_stream_player_3d.play()
+		
